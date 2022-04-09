3fbd870957b8aee08b6784e856f21e999547a91b
sergiogval <sergioval@gmail.com>
2022-04-09 15:37:01 -0500

    Update sys

--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebar');
-const methodOverride = require('express-override');
-const session = require(express-session);
+const app = require('app')
+// Server is listening
+app.listen(app.get("port"));
 
-// Inizializadores
-const app = express();
-
-// Settings
-app.set('port', process.env.PORT || 3000);
-app.set('view', path.join(__dirname, 'views'));
-app.engine('hbs', exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'),'layouts'),
-    partialsDirs: path.join(app.get('views'), 'partials'),
-    extname:'.hbd'
-}));
-app.set('view-engine', )
-
-// Middelwares
-app.use(express.urlencoder({extended:false}));
-app.use(methodOverride('_method'))
-app.use(session({
-    secret: 'myscecretapp',
-    resave: true,
-    saveUnitialized: true
-}));
-
-// Global variables
-
-// Routers
-app.use(require('./routers/index'))
-app.use(require('./routers/notes'))
-app.use(require('./routers/users'))
-
-// Statuc file
-app.use(express.static(path.join('__dirname', 'public')))
-
-//Server is listening
-app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
-});
-
-module.export = express
+console.log("Server on port", app.get("port"));
+console.log("Environment:", process.env.NODE_ENV);
--- src/routers/notes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routers = express.Router();
-
-router.get('/notes', (res, req) => {
-    res.send('Notes from database')
-})
-
-
-module.export = router;
